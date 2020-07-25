606b15bb15131086257f58e1076ad853be30a739
Claudiu <cduneata@gmail.com>
2020-07-25 18:52:57 +0300

    Add solution for assembly attributes

--- AssemblyAttrDemo/Program.cs
+++ AssemblyAttrDemo/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Reflection;
+
+[assembly: AssemblyDescription("This is a demo of the AssemblyDescription attribute.")]
 
 namespace AssemblyAttrDemo
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            Type type = typeof(AssemblyDescriptionAttribute);
+
+            object[] assemblyAttributes = assembly.GetCustomAttributes(type, false);
+
+            if(assemblyAttributes.Length > 0)
+            {
+                var description = (AssemblyDescriptionAttribute)assemblyAttributes[0];
+                Console.WriteLine("Description is: {0}", description);
+            }
         }
     }
 }
