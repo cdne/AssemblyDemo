ed1d5136ffb9518e52fa779d0e3cb2e745b3a026
Claudiu <cduneata@gmail.com>
2020-07-25 18:38:39 +0300

    Run current assembly

--- AssemblyDemo/Program.cs
+++ AssemblyDemo/Program.cs
@@ -7,16 +7,23 @@ namespace AssemblyDemo
     {
         static void Main(string[] args)
         {
+            Assembly assembly = Assembly.LoadFile(@"C:\Windows\Microsoft.NET\Framework\v4.0.30319\System.dll");
+            ShowAssembly(assembly);
+
+            Assembly executingAssembly = Assembly.GetExecutingAssembly();
+            ShowAssembly(executingAssembly);
+
+            Console.Read();
         }
 
         public static void ShowAssembly(Assembly assembly)
         {
-            Console.WriteLine($"Fullname: {assembly.FullName}, Location: {assembly.Location}, Cache: {assembly.GlobalAssemblyCache}, " +
-                $"ImageRunTimeVersion: {assembly.ImageRuntimeVersion}");
+            Console.WriteLine($"Fullname: {assembly.FullName}\nLocation: {assembly.Location}\nCache: {assembly.GlobalAssemblyCache}, " +
+                $"\nImageRunTimeVersion: {assembly.ImageRuntimeVersion}");
 
             foreach (var item in assembly.Modules)
             {
-                Console.WriteLine($"{item.Name}");
+                Console.WriteLine($"Module Name: {item.Name}");
             }
         }
     }
